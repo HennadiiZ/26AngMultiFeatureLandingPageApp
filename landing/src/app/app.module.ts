1811919f9ac69068e40a53a11de2be4c451a1f42
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-10-07 17:00:20 +0300

    444. Generating Modules + 445. Module Properties + 446. Connecting Modules

--- landing/src/app/app.module.ts
+++ landing/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherModule } from './weather/weather.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    WeatherModule
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+// 444. Generating Modules
+// 445. Module Properties
+// 446. Connecting Modules
+//
